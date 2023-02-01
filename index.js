ec5744b02260d2d086ffed089f457f0ba8b36c96
seongeon <dbtjsrjs8282@naver.com>
2023-02-01 20:52:04 +0900

    json 수정

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require("express");
 const product = require("./api/product");
 
 const app = express();
+const path = require("path");
 
 const PORT = process.env.PORT || 5050;
 
 // app.use("/api/product", product);
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "vercel-node-app/public")));
 
 app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: path.join(__dirname, "public") });
+  res.sendFile(path.join(__dirname, "vercel-node-app/public/index.html"));
 });
 
 // app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
