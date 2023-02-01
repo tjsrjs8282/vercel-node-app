fab8487c1f294b50860f55af1e3c5d4ab35294c6
seongeon <dbtjsrjs8282@naver.com>
2023-02-01 20:02:28 +0900

    nodemon 추가

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
 const app = express();
-
-const port = process.env.PORT || 3000;
+app.listen(process.env.PORT || 3000);
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile("index.html", { root: path.join(__dirname, "public") });
 });
+
+module.exports = app;
